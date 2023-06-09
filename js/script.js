1bb493abdf40b5a69bc21bd461b30d8285f47e96
patrictobola <tobola92@gmail.com>
2023-06-09 15:46:08 +0200

    done and tested layout in html, done css too

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log('JS OK')
+
+// Recupero gli elementi dal DOM 
+const countdownElement = document.getElementById('countdown');
+const numberListElement = document.getElementById('numberList');
